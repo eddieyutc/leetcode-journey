4c6662e216058a10ea54475c12f7cf55e0ea84a8
Eddie Yu <32135564+eddieyutc@users.noreply.github.com>
2023-05-22 20:50:32 +0100

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+  ignorePatterns: [
+    'out/',
+    '.eslintrc.js',
+    'jest.config.mjs',
+    'jest.node.mjs',
+  ],
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode journey
+
+Come see my journey on crushing leet code (or getting crushed by leet code).
+
+Credit to [NeetCode](https://youtu.be/8wysIxzqgPI) for providing the inspiration and a nice roadmap.
