4d76f246c846dba5370869ac88c59e1d422a6784
Eddie Yu <32135564+eddieyutc@users.noreply.github.com>
2023-05-27 20:18:16 +0100

    Add a cleaner and more concise solution to 876. Middle of Linked List

---
+++ challenges for new users/876. Middle of the Linked List/fast-slow-pointer-solution.ts
@@ -0,0 +1,13 @@
+import { ListNode } from '../../support-types/ListNode'
+
+function middleNode(head: ListNode | null): ListNode | null {
+  let fast = head
+  let slow = head
+  while (fast !== null && fast.next !== null) {
+    fast = fast?.next?.next ?? null
+    slow = slow?.next ?? null
+  }
+  return slow
+}
+
+export default middleNode
